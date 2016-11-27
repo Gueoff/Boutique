be2c168d8bc2585ed1b063586409ac451edd0203
Your name <you@example.com>
2016-11-27 04:03:57 +0100

    Ajout d'un premier test pour avoir les logs. log4j.properties + un test dans authenfication.java

--- application/src/main/java/application/Authentification.java
+++ application/src/main/java/application/Authentification.java
@@ -5,9 +5,15 @@ import com.alma.factories.impl.DAOFactoryImpl;
 import com.alma.model.Client;
 import com.alma.repositories.DAO;
 
+import org.apache.log4j.Logger;
+
 public class Authentification {
 
+	private static Logger logger = Logger.getLogger(Authentification.class.getName());
+
 	public boolean login(String login, String password){
+		logger.info("Authentification, login : " + login);
+		
 		DAOFactory factory = new DAOFactoryImpl();
 		DAO<Client> clientDao =  factory.getClientDAO();
 		Client client = clientDao.find(login);
@@ -18,8 +24,16 @@ public class Authentification {
 	}
 	
 	public void logup(Client client){
+		logger.info("Test2");
+
 		DAOFactory factory = new DAOFactoryImpl();
 		DAO<Client> clientDao =  factory.getClientDAO();
 		clientDao.create(client);
+	}
+	
+	public static void main(String[] args) {
+		Authentification a = new Authentification();
+		a.login("toto", "mo2pass");
+		//a.logup(null);
 	}
 }
